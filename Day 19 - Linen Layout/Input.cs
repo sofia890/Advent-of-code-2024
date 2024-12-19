0a5c5e08419fe0b2463d2f8087baa1e7705f7816
Sofia Carlsson <sofia.carlsson@sogeti.com>
2024-12-19 13:02:23 +0100

    Add day 19.

---
+++ Day 19 - Linen Layout/Input.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_19___Linen_Layout
+{
+    internal class Input
+    {
+        static (string[] towels, string[] patterns) Parse(string value)
+        {
+            var parts = value.Split("\r\n\r\n");
+            return (parts[0].Split(", "), parts[1].Split("\r\n"));
+        }
+        public static (string[] towels, string[] patterns) GetData()
+        {
+            return Parse(File.ReadAllText("Data/Input.txt"));
+        }
+        public static (string[] towels, string[] patterns) GetTrainingData()
+        {
+            return Parse(File.ReadAllText("Data/TrainingInput.txt"));
+        }
+    }
+}
---
+++ Day 19 - Linen Layout/Program.cs
@@ -0,0 +1,83 @@
+﻿using Day_19___Linen_Layout;
+
+using AdventLibrary;
+
+bool PatternIsPossible(string[] towels, string pattern)
+{
+    var workQueue = new PriorityQueue<string, int>();
+    workQueue.Enqueue(pattern, 0);
+
+    while (workQueue.Count > 0)
+    {
+        var remaining = workQueue.Dequeue();
+
+        var choices = towels.Where(x => x.Length <= remaining.Length)
+                            .Where(remaining.StartsWith)
+                            .Select((x, index) => (x, index));
+
+        foreach ((var towel, var index) in choices)
+        {
+            remaining = remaining[towel.Length..];
+
+            if (remaining.Length == 0)
+            {
+                return true;
+            }
+            else
+            {
+                workQueue.Enqueue(remaining, remaining.Length);
+            }
+        }
+    }
+
+    return false;
+}
+long PossiblePermutations(string[] towels, string pattern)
+{
+    (IEnumerable<string>, long) Expand(string remaining)
+    {
+        long nrofDone = 0;
+
+        var newItems = new List<string>();
+
+        var choices = towels.Where(x => x.Length <= remaining.Length)
+                            .Where(remaining.StartsWith)
+                            .Select((x, index) => (x, index));
+
+        foreach ((var towel, var index) in choices)
+        {
+            var newItem = remaining[towel.Length..];
+
+            if (newItem.Length == 0)
+            {
+                nrofDone++;
+            }
+            else
+            {
+                newItems.Add(newItem);
+            }
+        }
+
+        return (newItems, nrofDone);
+    }
+
+    return Permutations.Count(pattern, 0L, x => int.MaxValue - x.Length, Expand, x => x);
+}
+void PartOne(string[] towels, string[] patterns)
+{
+    var nrofMatches = patterns.AsParallel()
+                              .Where(pattern => PatternIsPossible(towels, pattern))
+                              .Count();
+    Console.WriteLine($"Part One: Number of feasible patterns are {nrofMatches}.");
+}
+void PartTwo(string[] towels, string[] patterns)
+{
+    var nrofOptions = patterns.AsParallel()
+                              .Select(pattern => PossiblePermutations(towels, pattern))
+                              .Aggregate(0L, (a, b) => a + b);
+    Console.WriteLine($"Part Two: Number of feasible permutations are {nrofOptions}.");
+}
+
+(var towels, var patterns) = Input.GetData();
+PartOne(towels, patterns);
+PartTwo(towels, patterns);
