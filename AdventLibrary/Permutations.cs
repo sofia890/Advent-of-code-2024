26d653616984f9991e8dd7d158b2b7e219d9fc26
Sofia Carlsson <sofia.carlsson@sogeti.com>
2024-12-19 13:02:13 +0100

    Add helper functions for permutations

---
+++ AdventLibrary/Permutations.cs
@@ -0,0 +1,59 @@
+﻿using System.Data.SqlTypes;
+using System.Numerics;
+
+namespace AdventLibrary
+{
+    public static class Permutations
+    {
+        private class WorkItem<RefDataType, RefCountType>(RefDataType value, RefCountType count)
+        {
+            public RefDataType Value { get; } = value;
+            public RefCountType Count { get; set; } = count;
+        }
+        public static RefCountType Count<RefDataType, RefKeyType, RefPriorityType, RefCountType>(
+            RefDataType initialValue,
+            RefCountType seed,
+            Func<RefDataType, RefPriorityType> getPriority,
+            Func<RefDataType, (IEnumerable<RefDataType> newElements, RefCountType nrofDone)> expand,
+            Func<RefDataType, RefKeyType> getKey
+        )
+            where RefDataType : notnull
+            where RefKeyType : notnull
+            where RefPriorityType : INumber<RefPriorityType>
+            where RefCountType : INumber<RefCountType>
+        {
+            var lookup = new Dictionary<RefKeyType, WorkItem<RefDataType, RefCountType>>();
+
+            var counter = seed;
+
+            var workQueue = new PriorityQueue<WorkItem<RefDataType, RefCountType>, RefPriorityType>();
+            workQueue.Enqueue(new(initialValue, RefCountType.One), getPriority(initialValue));
+
+            while (workQueue.Count > 0)
+            {
+                var item = workQueue.Dequeue();
+                (var newElements, var nrOfDone) = expand(item.Value);
+
+                counter += nrOfDone * item.Count;
+
+                foreach (var element in newElements)
+                {
+                    var key = getKey(element);
+
+                    if (lookup.ContainsKey(key))
+                    {
+                        lookup[getKey(element)].Count += item.Count;
+                    }
+                    else
+                    {
+                        var newItem = new WorkItem<RefDataType, RefCountType>(element, item.Count);
+                        lookup.Add(key, newItem);
+                        workQueue.Enqueue(newItem, getPriority(element));
+                    }
+                }
+            }
+
+            return counter;
+        }
+    }
+}
