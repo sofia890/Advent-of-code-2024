cd63f268f886779c1761a372c34ad78883ab13a5
Sofia Carlsson <sofia.carlsson@sogeti.com>
2024-12-30 11:17:14 +0100

    Cleanup day 2.

---
+++ Day 2 - Red-Nosed Reports/Input.cs
@@ -0,0 +1,23 @@
+﻿namespace Day_2___Red_Nosed_Reports
+{
+    internal static class Input
+    {
+        static List<int[]> Parse(string filePath)
+        {
+            return File.ReadAllText(filePath)
+                       .Split("\r\n")
+                       .Select(x => x.Split(' ')
+                                     .Select(int.Parse)
+                                     .ToArray())
+                       .ToList();
+        }
+        public static List<int[]> GetData()
+        {
+            return Parse("Data/Input.txt");
+        }
+        public static List<int[]> GetTrainingData()
+        {
+            return Parse("Data/TrainingInput.txt");
+        }
+    }
+}
